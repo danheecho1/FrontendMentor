055a1e98dae82eeade73b0d7a00374ca194fd352
Danny Cho <danheecho@gmail.com>
2023-01-27 22:58:35 -0800

    Mobile view finished
    Added another variable in JS to bold the text when expanded. Added a few classes to style the question text and the arrow when expanded.

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,10 +1,12 @@
-let questions = document.getElementsByClassName("question-div");
+let questionsDiv = document.getElementsByClassName("question-div");
+let questions = document.getElementsByClassName("question")
 let answers = document.querySelectorAll(".answer");
 let arrows = document.querySelectorAll(".question-div img")
 
-for(let i = 0; i < questions.length; i++) {
-	questions[i].addEventListener("click", function() {
-		answers[i].classList.toggle("active");
-		arrows[i].classList.toggle("expanded");
+for(let i = 0; i < questionsDiv.length; i++) {
+	questionsDiv[i].addEventListener("click", function() {
+		questions[i].classList.toggle("active-question")
+		answers[i].classList.toggle("active-answer");
+		arrows[i].classList.toggle("active-arrow");
 	});
 }
