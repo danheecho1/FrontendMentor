6b3cd8ee59456d4ad19b97c67a9d4800fd64806d
Danny Cho <danheecho@gmail.com>
2023-02-10 18:30:38 -0800

    Overall HTML content and JS done; CSS WIP
    Clicking the button will populate the advice # and content. Still need to correct styling and probably the placement of the button.

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,8 +1,11 @@
+const number = document.querySelector(".viewport__card__header__id")
+const quote = document.querySelector(".viewport__card__advice");
+
 function getAdvice() {
 	fetch("https://api.adviceslip.com/advice")
 		.then((response) => response.json())
 		.then((data) => {
-			const markup = document.getElementById("advice");
-			markup.innerHTML = data["slip"]["advice"];
+			number.innerHTML = data["slip"]["id"];
+			quote.innerHTML = '"' + data["slip"]["advice"] + '"';
 		});
 }
