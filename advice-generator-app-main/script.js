e9779938cb7ea21cebac9f8587a217a0193dfbde
Danny Cho <danheecho@gmail.com>
2023-02-10 22:17:52 -0800

    Desktop view done
    Also commented ChatGPT's refactored version of the JS code and my notes in the readme file. Minor styling changes in CSS and HTML.

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,11 +1,29 @@
-const number = document.querySelector(".viewport__card__header__id")
+const number = document.querySelector(".viewport__card__header__id");
 const quote = document.querySelector(".viewport__card__advice");
 
 function getAdvice() {
 	fetch("https://api.adviceslip.com/advice")
 		.then((response) => response.json())
 		.then((data) => {
-			number.innerHTML = data["slip"]["id"];
+			number.innerHTML = "  #  " + data["slip"]["id"];
 			quote.innerHTML = '"' + data["slip"]["advice"] + '"';
 		});
 }
+
+/* 
+THIS IS CHAT GPT'S VERSION OF REFACTORED CODE
+
+const numberElement = document.querySelector(".viewport__card__header__id");
+const quoteElement = document.querySelector(".viewport__card__advice");
+
+async function getAdvice() {
+	try {
+		const response = await fetch("https://api.adviceslip.com/advice");
+		const data = await response.json();
+		numberElement.innerHTML = `  #  ${data.slip.id}`;
+		quoteElement.innerHTML = `"${data.slip.advice}"`;
+	} catch (error) {
+		console.error(error);
+	}
+}
+*/
